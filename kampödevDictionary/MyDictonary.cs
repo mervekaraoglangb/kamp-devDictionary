165de233d66ee76537b92bdf7a64a08057b2c2ec
Hp <Hp@LAPTOP-DI49FIR8>
2021-01-28 21:00:30 +0300

    Add project files.

---
+++ kampödevDictionary/MyDictonary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kampödevDictionary
+{
+    class MyDictonary<Mkey,Kvalue>
+    {
+        private Mkey[] keys;
+        private Kvalue[] values;
+        public MyDictonary()
+        {
+            keys = new Mkey[0];
+            values = new Kvalue[0];
+        }
+        public void Add(Mkey key ,Kvalue value)
+        {
+            Mkey[] tempkey = keys;
+            Kvalue[] tempvalue = values;
+
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                keys[i] = tempkey[i];
+            }
+
+            for (int i = 0; i < tempvalue.Length; i++)
+            {
+                values[i] = tempvalue[i];
+            }
+            
+
+            Console.WriteLine("Key : " + key + " and Value : " + value );
+        }
+    }
+}
---
+++ kampödevDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kampödevDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> kullanicilar = new MyDictonary<int, string>();
+
+            kullanicilar.Add(0, "Merve KARAOĞLAN");
+            kullanicilar.Add(1, "Emrullah ORHAN");
+            kullanicilar.Add(2, "Emine BOZOĞLAN");
+
+            
+        }
+    }
+}
